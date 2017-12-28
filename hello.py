c2ef9c76cb73d6f68934a07e1aceb20964fdc83e
Daniel Arenas <daniel_6287@hotmail.com>
2017-12-28 09:42:20 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 import tweepy 
 
-consumer_key = '5cx9dQAsH6kRytnSkKo94lr9s'
-consumer_secret = 'GDrNkfkexLcPK63rGccvnzcEMKnjR5bSVKm9v4buMKPy4Y3kXn'
-access_token = '3193851775-ymak09P7b0Vz5GxEk6UKy2nuu2QzmpVRvMcFo5f'
-access_token_secret = 'EuYeGiPLaEDrpem16prBz6W9x7jtadfkudKKigoP2eRk7'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
